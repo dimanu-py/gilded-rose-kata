2b88fc5a5bb0b91e4ae0d7186548a98f8c1a96a0
dimanu-py <diegomtz126@gmail.com>
2024-05-30 14:21:26 +0200

    test(gilded-rose): initial approval test for update quality method

---
+++ tests/test_gilded_rose_approvals.py
@@ -0,0 +1,20 @@
+from approvaltests import verify
+
+from gilded_rose_approvals.gilded_rose import Item, GildedRose
+
+
+
+def item_printer(item: Item) -> str:
+    return f"{item.name}, sell_in: {item.sell_in}, quality: {item.quality}"
+
+
+class TestGildedRoseApprovals:
+
+    def test_update_quality(self, intellij_diff_reporter):
+        item = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(item)
+
+        gilded_rose.update_quality()
+
+        item_as_string = item_printer(item[0])
+        verify(item_as_string, reporter=intellij_diff_reporter)
