90968badee56ef8f050e0ca9ec8500d0c04b42dc
dimanu-py <diegomtz126@gmail.com>
2024-05-31 18:08:54 +0200

    refactor(gilded-rose): use new BackstagePassesItem class when item is of this type

--- tests/test_gilded_rose_approvals.py
+++ tests/test_gilded_rose_approvals.py
@@ -1,7 +1,7 @@
 from approvaltests import verify_all_combinations
 
 from gilded_rose_approvals.gilded_rose import GildedRose
-from gilded_rose_approvals.items import Item, CommonItem, AgedBrieItem
+from gilded_rose_approvals.items import Item, CommonItem, AgedBrieItem, BackstagePassesItem
 
 
 def item_printer(item: Item) -> str:
@@ -36,4 +36,6 @@ class TestGildedRoseApprovals:
             return CommonItem(name, sell_in, quality)
         elif name == "Aged Brie":
             return AgedBrieItem(name, sell_in, quality)
+        elif name == "Backstage passes":
+            return BackstagePassesItem(name, sell_in, quality)
         return Item(name, sell_in, quality)
